f6e7344130de7ed26f4b7ee94d72808e87b3174c
gesifeta <adamgemechu@gmail.com>
2025-07-18 08:45:46 +0300

    route varibales

--- hello.py
+++ hello.py
@@ -3,7 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
     return "Hello flask."
-index()
+
+@app.route('/greeting/<name>')
+def greet(name):
+    return "Hello " + name
+@app.route("/bye")
+def say_bye():
+    return "Bye!"
+if __name__ == '__main__':
+    app.run(debug=True)
